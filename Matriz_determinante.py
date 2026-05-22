72ff93f9b1bcd4ace8aa368f96f696144aca7248
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-22 17:59:58 -0500

    Comentarios para mejor entendimiento

--- Matriz_determinante.py
+++ Matriz_determinante.py
@@ -1,4 +1,6 @@
+# Importamos random para que de uno al 20 sea de manera desordenada
 import random
+# Inportamos estos para hacer facilmente el determinante
 import numpy as np
 
 filas = int(input("Ingrese el número de filas: "))
@@ -9,14 +11,18 @@ matriz = []
 for i in range(filas):
     fila = []
     for j in range(columnas):
+# Agregamos que numeros randoms saldran
         fila.append(random.randint(1, 20))
     matriz.append(fila)
+
 
 print("Matriz que generamos para ti es:")
 for fila in matriz:
     print(fila)
 
+# Se puede hacer la operración de determinante solo si las filas son las mismas que las columnas
 if filas == columnas:
+# Se utiliza la operacion ya que con diferentes matrices son diferentes procesos 
     determinante = int(round(np.linalg.det(np.array(matriz))))
     print("Determinante:", determinante)
 else:
