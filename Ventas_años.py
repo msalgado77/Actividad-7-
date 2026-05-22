ba0a8c8b822969aeecc2670ab8ed7c572cee521b
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-22 18:12:37 -0500

    Comentarios para mejor entendimiento

--- Ventas_años.py
+++ Ventas_años.py
@@ -1,15 +1,18 @@
+# Inportamos estos para hacer facilmente unos procesos
 import numpy as np
 
 vendedores = int(input("Ingrese el número de empleados que analizaremos: "))
 años = int(input("Ingrese los años de operación de cada empleado: "))
 
+# Se forma una matriz de zeros
 ventas = np.zeros((vendedores, años))
 
 for i in range(vendedores):
-    print(f"\n--- Vendedor {i+1} ---")
+    print(f"Vendedor {i+1} ")
     for j in range(años):
-        ventas[i, j] = float(input(f"Ingrese las ventas del año:"))
+        ventas[i, j] = float(input("Ingrese las ventas del año:"))
 
+# Sumar Filas con el axis
 total_vendedores = ventas.sum(axis=1)
 total_años = ventas.sum(axis=0)
 gran_total = ventas.sum()
