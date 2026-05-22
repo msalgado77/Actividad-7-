aad27c7adffb15f524f0ea430828df58423a4ddd
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-22 14:45:16 -0500

    Interacción usuario

--- Metodo_burbuja.py
+++ Metodo_burbuja.py
@@ -8,3 +8,14 @@ def metodo_burbuja(lista):
                 lista[j] = lista[j + 1]
                 lista[j + 1] = cambio
     return lista
+
+
+# Entrada de datos del usuario
+print("Ingresa los números que quieras ordenar")
+entrada = input("Acuerdate que es separado por espacios: ")
+
+numeros = list(map(int, entrada.split()))
+
+print("Lista original:", numeros)
+ordenada = metodo_burbuja(numeros)
+print("Lista ordenada (mayor a menor):", ordenada)
