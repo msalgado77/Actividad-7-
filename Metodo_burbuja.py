299f2e62fbfc72260003afe7b5d1102e41c2a9da
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-05-22 17:47:05 -0500

    Comentarios para mejor entendimiento

--- Metodo_burbuja.py
+++ Metodo_burbuja.py
@@ -1,8 +1,10 @@
 # Codigo del metodo burbuja
 def metodo_burbuja(lista):
     n = len(lista)
+# Creando una lista con los datos que se ingresan
     for i in range(1, n):
         for j in range(0, n - 1):
+# Es un ciclo hasta que se complete la n ingresada (datos)
             if lista[j] < lista[j + 1]:
                 cambio = lista[j]
                 lista[j] = lista[j + 1]
@@ -14,6 +16,7 @@ def metodo_burbuja(lista):
 print("Ingresa los números que quieras ordenar")
 entrada = input("Acuerdate que es separado por espacios: ")
 
+# Lista de datos enteros
 numeros = list(map(int, entrada.split()))
 
 print("Lista original:", numeros)
